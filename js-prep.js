7ed90b2380053080752cb411f67c808603c84877
vijay5599 <vbvijay84@gamil.com>
2023-04-18 08:42:14 +0530

    Main

--- js-prep.js
+++ js-prep.js
@@ -102,4 +102,15 @@
 
 
 /* count the number of vovels */
+function countVowels(str){
+    var vowel_list = "aeiouAEIOU";
+    var vcount = 0;
+    for(var i = 0; i<str.length; i++){
+        if(vowel_list.indexOf(str[i])  !== -1 ){
+            vcount += 1;
+        }
+    }
+    return vcount;
+}
 
+console.log(countVowels("I love You"));
