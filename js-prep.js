84ff2703cd50cfb3e5f730c2db799fbc2e0f0c1b
vijay5599 <59635616+vijay5599@users.noreply.github.com>
2023-05-02 16:15:29 +0530

    Update js-prep.js

--- js-prep.js
+++ js-prep.js
@@ -342,3 +342,27 @@ let arr = [ 10, 90, 49, 2, 1, 5, 23 ];
 let n = arr.length;
 console.log(sortInWave(arr, n));
 
+
+
+
+/*Maximum of all subarrays of size k*/
+function printKMax(arr,n,k)
+{
+    let j, max;
+    for (let i = 0; i <= n - k; i++)
+    {
+        max = arr[i];
+        for (j = 1; j < k; j++)
+        {
+            if (arr[i + j] > max)
+                max = arr[i + j];
+        }
+        document.write( max + " ");
+    }
+}
+    let arr = [1, 2, 3, 1, 4, 5, 2, 3, 6];
+    let n =arr.length;
+    let k = 3;
+    printKMax(arr, n, k);
+
+
