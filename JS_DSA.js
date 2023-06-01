fc03d71ec19f27ad39a1f120de36e8779a0bf7aa
vijay5599 <59635616+vijay5599@users.noreply.github.com>
2023-06-01 08:46:31 +0530

    Update JS_DSA.js

--- JS_DSA.js
+++ JS_DSA.js
@@ -134,6 +134,52 @@ console.log(rmvDuplicates(arr, n))
 
 
 
+/////////////////////////////////////////
+Practice - 4 Product Array 
+
+function productArray(arr, n)
+    {
+        // Initialize memory to all arrays
+        let left = new Array(n);
+        let right = new Array(n);
+        let prod = new Array(n);
+ 
+        let i, j;
+ 
+        /* Left most element of left array
+        is always 1 */
+        left[0] = 1;
+ 
+        /* Right most element of right
+        array is always 1 */
+        right[n - 1] = 1;
+ 
+        /* Construct the left array */
+        for (i = 1; i < n; i++)
+            left[i] = arr[i - 1] * left[i - 1];
+       console.log("left array", left)
+ 
+        /* Construct the right array */
+        for (j = n - 2; j >= 0; j--)
+            right[j] = arr[j + 1] * right[j + 1];
+        console.log("right array", right)
+ 
+        /* Construct the product array using
+        left[] and right[] */
+        for (i = 0; i < n; i++)
+            prod[i] = left[i] * right[i];
+ 
+ 
+        return prod
+    
+}
+
+const nums = [10, 3, 5, 6, 2]
+const n = nums.length
+console.log(productArray(nums, n))
+
+
+
 
 
 
